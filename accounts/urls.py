30ff8b876a2d7a36a8ed4a42e692e6464bdfa75c
Toshiba <gmateusz92@gmail.com>
2022-09-11 17:32:09 +0200

    i

---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('', views.home, name='home'),
+    path('register', views.register, name='register'),
+    path('login1', views.login1, name='login1'),
+    path('logout', views.logoutUser, name='logout')
+]
---
+++ accounts/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth.models import User, auth
+from .forms import CreateUserForm
+from django.contrib.auth import authenticate, login, logout, logout as auth_logout
+from django.http import HttpResponseRedirect
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def register(request):
+    if request.user.is_authenticated:
+        return redirect('home')
+    else:
+        form = CreateUserForm()
+        if request.method == 'POST':
+            form = CreateUserForm(request.POST)
+            if form.is_valid():
+                form.save()
+                user = form.cleaned_data.get('username')
+                messages.success(request, 'Account was created for ' + user)
+
+                return redirect('login')
+
+        context = {'form': form}
+        return render(request, 'register.html', context)
+
+
+def login1(request):
+	if request.user.is_authenticated:
+		return redirect('home')
+	else:
+		if request.method == 'POST':
+			username = request.POST.get('username')
+			password = request.POST.get('password')
+
+			user = authenticate(request, username=username, password=password)
+
+			if user is not None:
+				login(request, user)
+				return redirect('home')
+			else:
+				messages.info(request, 'Username OR password is incorrect')
+
+		context = {}
+		return render(request, 'login1.html', context)
+
+def logoutUser(request):
+	logout(request)
+	return redirect('login1')
